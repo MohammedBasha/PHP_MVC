937f3d56f20ac00b4f145259bdf69403a069a3bc
MohammedBasha <me@mohammedbasha.com>
2021-08-29 08:26:12 +0200

    Start the PHP MVC Course

---
+++ README.md
@@ -0,0 +1,3 @@
+## PHP 5.6 and MySQL 5.7 (MVC)
+
+[Mohammed Yehya PHP 5.6 and MySQL 5.7](https://www.youtube.com/watch?v=ehiitHAqW9A&list=PLrwRNJX9gLs3kkSDgCHFlpgL6qLrlHUBG&index=72&ab_channel=%D9%85%D8%AD%D9%85%D8%AF%D9%8A%D8%AD%D9%8A%D9%89)
---
+++ mvc.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * MVC: A pattern used to organize the code
+ * M = Model: Business rules - Work Flows - The main Classes that deal with the data (DB - Files - Web Service)
+ * V = View: The output files that will be shown to the user
+ * C = Controller: it receives a request and look for the appropriate View to load it, if the View needs some data, then the Controller talk to the Model (Business) to deal with the data source, do the operations, and send it again to the view.
+ */
+
